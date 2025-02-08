726f79d973b3136dc6d4b7a56ded11614583eafc
sali223 <120015635+sali223@users.noreply.github.com>
2025-02-07 23:04:24 -0600

    Create car.py

---
+++ car.py
@@ -0,0 +1,35 @@
+cars = [
+    "Toyota Corolla",
+    "Honda Civic",
+    "Ford Mustang",
+    "Chevrolet Camaro",
+    "Tesla Model S",
+    "BMW 3 Series",
+    "Mercedes-Benz C-Class",
+    "Audi A4",
+    "Nissan Altima",
+    "Volkswagen Golf",
+    "Porsche 911",
+    "Lamborghini Aventador",
+    "Ferrari 488",
+    "Maserati Ghibli",
+    "Jaguar F-Type",
+    "Subaru Impreza",
+    "Hyundai Sonata",
+    "Kia Stinger",
+    "Mazda MX-5 Miata",
+    "Chrysler 300",
+    "Dodge Charger",
+    "Acura MDX",
+    "Cadillac Escalade",
+    "Buick Enclave",
+    "Land Rover Range Rover"
+]
+
+#serch for a car
+search_car = "Ferrari 488"
+
+if search_car in cars:
+    print(f"{search_car} is in the list.")
+else:
+    print(f"{search_car} is not in the list.")
